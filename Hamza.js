961f5d748273f5931ef0905618ee35ba8b95c62f
imhamzamoeen <60896843+imhamzamoeen@users.noreply.github.com>
2020-04-09 20:24:37 +0500

    Add files via upload

---
+++ Hamza.js
@@ -0,0 +1,36 @@
+import React,{Component} from 'react';
+import ReactDOM from 'react-dom';
+import { Helmet } from 'react-helmet';
+class App extends Component {
+render() {
+return (
+<div>
+        <Helmet>
+          <title>Hamza Moeen</title>
+        </Helmet>
+<h1>Hamza Moeen</h1>
+<h3><strong>It’s Not Your Job to Tell Yourself “No”</strong></h3>
+<p>Have you ever told yourself no? I definitely have.
+I'm talking about thoughts like…
+“Why bother applying? I doubt that school would ever accept me.”
+“This business idea isn't going to work. I'm too old/too young/too inexperienced.”
+“He’d never date me. I might as well move on.”
+<br/>
+“My boss won't promote me anyway. Why should I even ask?”
+From what I can tell, we all doubt ourselves from time to time. The problem, of course, is that telling yourself no prevents you from getting started at all. When you decide not to act, you deny yourself opportunities.
+Once I realized this, I started following a simple little rule that has helped me stick to things and persevere when I normally would have given up.
+Here's how it works and why you should use it…
+Don’t Give Up At The First “No”
+<br/>
+Most of us will do just about anything to avoid failure. (Why do you think celebrities, congressmen, and athletes say and do so many stupid things to cover up scandals of all types? They desperately want to avoid failure as long as possible.)
+I think this is natural. Nobody wants to fail. On a smaller scale, you and I also want to prevent failure. And that's why we come up with reasons for why we shouldn't do things that we want to do. And it's also why we abandon our ideas as soon as we get any type of negative feedback.
+But here's the thing…
+No rarely means impossible. No rarely means never. Usually, if someone tells you “no” what they really mean is “not right now” or “not in that way.”
+Keeping that in mind, I’ve started to follow a simple rule that helps me get past negative feedback and gives me a little bit more perseverance when I would normally call it quits.
+<br/>
+Here’s the rule: Don’t give up at the first, “No.” </p>
+</div>
+); }
+}
+export default App;
+
